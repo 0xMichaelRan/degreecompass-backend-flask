68a3dd3e1b119d345d45a29cb968bee21633f570
0xMichaelRan <michaelran16@gmail.com>
2023-12-19 05:04:39 +0000

    fix CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.major_routes import major_bp
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(major_bp, url_prefix='/api')
 
 if __name__ == '__main__':

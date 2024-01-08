2d079b790f6264cce19fe26585590e2464adc4a2
0xMichaelRan <michaelran16@gmail.com>
2024-01-08 09:41:26 +0000

    change /ask to chinese prompt

--- prompts/major_prompts.py
+++ prompts/major_prompts.py
@@ -1,4 +1,3 @@
-
 def get_major_intro_prompt(major_info):
     return f"""
     我是一名大学申请者，对大学专业不太了解。请用书面语，并以适当的Markdown格式为我提供关于【{major_info['major_name']}】的一般信息，包括以下几个方面：
@@ -9,7 +8,9 @@ def get_major_intro_prompt(major_info):
 
     就业机会：学习【{major_info['major_name']}】的学生在毕业后有哪些常见的就业方向和职业机会？行业前景如何？
 
-    控制总字数在200字以内，不需要标题，不要用列表形式。
+    高校选择：哪些著名高校的这个专业比较著名？有哪些知名校友？
+
+    不要提到“前景广阔”，“发展空间较大”之类的词语，用中肯的语气，偏年轻化的语言，控制总字数在200字以内，不需要标题，不要用列表形式。
     """ 
 
 def get_major_qa_prompt(major_info):
@@ -36,4 +37,16 @@ def get_major_qa_prompt(major_info):
     2. 答案中如有单引号需要用两个单引号转义
     3. 每个答案控制在100-200字之间
     4. 不要包含markdown格式或其他额外内容
+    """
+
+def get_major_ask_prompt(context, question):
+    return f"""
+    我是一名大学申请者，对【{context['major_name']}】专业有一些疑问。请根据以下背景信息回答我的问题：
+
+    专业介绍：
+    {context['intro_content']}
+
+    我的问题是：{question}
+
+    请提供准确且有帮助的回答。如果问题无法完全根据提供的背景信息回答，请说明这一点，并提供与该专业相关的一般性指导。请用中肯的语气，偏年轻化的语言回答，控制在100字以内。不要使用"前景广阔"，"发展空间较大"之类的词语。
     """

1733cd26a0ff409eaa5dff52ad8506160fa98f6e
0xMichaelRan <michaelran16@gmail.com>
2024-11-13 14:29:57 +0800

    update backend port and docker postgres version

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.major_routes import major_bp
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -8,4 +13,6 @@ CORS(app)
 app.register_blueprint(major_bp, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the API port from environment variables, default to 5000 if not set
+    port = int(os.getenv('BACKEND_PORT', 5000))
+    app.run(debug=True, port=port)

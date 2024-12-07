84ee109cb4e5bcd66cee75611b83f60435768318
0xMichaelRan <michaelran16@gmail.com>
2024-12-07 13:37:39 +0800

    rename docker container

--- run-backend.sh
+++ run-backend.sh
@@ -1,5 +1,5 @@
 echo -ne "\033]0;hub/degree\007"
-cd /usr/local/hub/degree-compass/backend-flask
+cd /usr/local/hub/degree/backend-flask
 docker compose up -d
 poetry install
 poetry run python app.py
